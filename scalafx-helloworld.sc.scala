11246e5af5fddfd5b4ee1052e74e1ed30db2ae3f
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2026-07-16 14:04:49 -0400

    updated

--- scalafx-helloworld.sc.scala
+++ scalafx-helloworld.sc.scala
@@ -3,20 +3,33 @@
 import scalafx.application.JFXApp3
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
-import scalafx.scene.control.Label
-import scalafx.scene.layout.BorderPane
+import scalafx.scene.control.{Label,Button}
+import scalafx.scene.layout.{BorderPane, GridPane}
 
 object HelloWorld extends JFXApp3 :
 
   override def start(): Unit = {
+        // 1. Create the Grid container
+    val grid = new GridPane {
+      hgap = 10
+      vgap = 10
+      padding = Insets(20)
+    }  
+
+    grid.add  (new Button("Click Me")  {
+        onAction = _ => println("Hello World")
+    },0,0)
+
+    grid.add(new Label("Hello World " +
+        "and welcome to ScalaFX!"), 0, 1)  
+
     stage = new JFXApp3.PrimaryStage {
     title = "Hello"
     scene = new Scene {
-        root = new BorderPane {
-        padding = Insets(75)
-        center = new Label("Hello World")
-        }
-    }
+      root = new BorderPane {
+      padding = Insets(75)
+      center = grid
+      }
+      }
     }
   }
-
